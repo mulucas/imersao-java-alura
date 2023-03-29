74b88b57da89edb8720bd86c295c99193204738d
Lucas Queiroz <lukasqueiroz54@gmail.com>
2023-03-29 16:05:23 -0300

    Consumindo uma API de filmes com Java

---
+++ src/App.java
@@ -0,0 +1,35 @@
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.sql.ClientInfoStatus;
+import java.util.List;
+import java.util.Map;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        // Faz uma conexão HTTP e busca os dados do endereco passado
+        String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json";
+        URI endereco = URI.create(url);
+        var client = HttpClient.newHttpClient();
+        var request = HttpRequest.newBuilder(endereco).GET().build();
+        HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+        String body = response.body();
+        //System.out.println(body);
+
+        // Extrair só os dados que nos interessa (titulo, poster, classificação)
+        var parse = new JsonParser();
+        List<Map<String, String>> listaDeFilmes = parse.parse(body);
+        
+        // Exibir os dados
+        for (Map<String,String> filme : listaDeFilmes) {
+            System.out.println(filme.get("title"));
+            System.out.println(filme.get("image"));
+            System.out.println(filme.get("imDbRating"));
+        }
+
+    }
+
+}
